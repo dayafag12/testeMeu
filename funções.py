44f001f063f5e0050d3a72acfe8d8dd157fcd2a2
dayafag12 <109545517+dayafag12@users.noreply.github.com>
2022-07-18 14:26:37 -0300

    Update funções.py

--- funções.py
+++ funções.py
@@ -2,3 +2,4 @@ from camp_function import *
 print("teste aqui pra ver se rola disso dar certo em")
 # OpenDealPool('1mASRUOvyjJ0dtU43xhnKj4iEN02uWypxIQsXWAHSXRA','2022-05-02','2022-05-06',False)
 templatedf(20)
+print("Hello Word")
